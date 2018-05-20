4a2b7b3b6696f45ffe0cfb602ff064c4a22cd501
Juaquin <Noahdavidian@hotmail.dk>
2018-05-20 22:05:47 +0200

    Added Prefabs and working on Level
    working on some Level Design! Lets see how far it goes!

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+    public AudioSource audioSrc;
+
+
+    [Header("Audio Clips")]
+    public AudioClip gameOver;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+
+// GameObject.FindGameObjectWithTag("AudioManager").SendMessage("PlayBossSpawnSound"); USE THIS FOR SOUND REFERENCE IN
+// OTHER SCRIPTS
