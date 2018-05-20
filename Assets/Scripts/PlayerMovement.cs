b327d2e3239e5e8b4a44c945ed0eb51438ce397d
Juaquin <Noahdavidian@hotmail.dk>
2018-05-20 21:07:30 +0200

    First Commit!
    Hopefully this one works!

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Rigidbody rb;
+    public float forwardForce = 0f; // Our force variable, so we can edit in inspector
+    public float sidewaysForce = 0f;
+
+	
+	// FixedUpdate because we mess with physics
+	void FixedUpdate () {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime); // Adding force of 2000 on the z-axis, with help from the variable
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
+
+    }
+}
+
