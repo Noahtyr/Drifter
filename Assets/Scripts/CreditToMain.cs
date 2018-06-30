876d6c55af9cad2cb3d58d6bbbaec8b80dca2b95
Juaquin <Noahdavidian@hotmail.dk>
2018-06-30 16:17:06 +0200

    Made more changes

---
+++ Assets/Scripts/CreditToMain.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class CreditToMain : MonoBehaviour {
+
+	public void BackToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
