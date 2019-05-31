ccad1f52e2d27379199332b7c84d7eb2813579c0
Juaquin <Noahdavidian@hotmail.dk>
2019-05-31 21:15:29 +0200

    Removed Text mesh

---
+++ Assets/Scripts/Destroy.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour {
+
+	public GameObject destroyed;
+
+	void OnMouseDown(){
+		Instantiate (destroyed, transform.position, transform.rotation);
+}
+}
